9d0f7e9de7b279d1953d42450b193b1f246ad8d9
ko-ki <135305163+ZUMpou@users.noreply.github.com>
2023-06-21 15:46:22 +0900

    いお

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import json
 
+def save_post(title, content):
+    post = {"title": title, "content": content}
+    with open('posts.json', 'a') as file:
+        json.dump(post, file)
+        file.write('\n')
+
+def load_posts():
+    with open('posts.json', 'r') as file:
+        return [json.loads(line) for line in file]
+
 def main():
     st.title("掲示板アプリ")
 
@@ -11,7 +21,8 @@ def main():
     # 投稿ボタンが押された場合
     if st.button("投稿する") and new_post_title and new_post_content:
         new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
-    　  save_post(new_post_title, new_post_content)
+        
+        save_post(new_post_title, new_post_content)
         st.success("投稿が保存されました！")
 
     # 保存された投稿の表示
@@ -25,3 +36,6 @@ def main():
             st.text(post["title"])
             st.text(post["content"])
             st.markdown("---")
+
+if __name__ == "__main__":
+    main()
