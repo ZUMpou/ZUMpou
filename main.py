8ac70d922500bc00498600803f0e6251659bd485
ko-ki <135305163+ZUMpou@users.noreply.github.com>
2023-06-21 16:26:38 +0900

    おう

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import streamlit as st
 import json
 import datetime
-
-if st.button("投稿する") and new_post_title and new_post_content:
-    new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
+ new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
     if "＠" in new_post_title or "＠" in new_post_content:
         st.warning("禁止ワードが含まれています！")
 
     save_post(new_post_title, new_post_content)
     st.success("投稿が保存されました！")
-    st.write("投稿時間:", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
-
+    st.write("投稿時間:", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 # 禁止ワードのリスト
 banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
 
