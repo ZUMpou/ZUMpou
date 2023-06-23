a0a4a76b53ba7e9aad7231998140abab4297f04c
ko-ki <135305163+ZUMpou@users.noreply.github.com>
2023-06-23 14:20:35 +0900

    i

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import datetime
 
 if "＠" in new_post_title or "＠" in new_post_content:
         st.warning("禁止ワードが含まれています！")
-
-    save_post(new_post_title, new_post_content)
-    st.success("投稿が保存されました！")
-    st.write("投稿時間:", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        save_post(new_post_title, new_post_content)
+        st.success("投稿が保存されました！")
+        st.write("投稿時間:", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 # 禁止ワードのリスト
 banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
 
