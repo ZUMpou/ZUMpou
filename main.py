92841d6559c4e117bc607d2fc02bdd2571d23ff7
ko-ki <135305163+ZUMpou@users.noreply.github.com>
2023-06-30 15:08:52 +0900

    ぉう

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print('banana')
+import streamlit as st
+import json
+from datetime import datetime
+import pytz
+import urllib.parse
+
+def main():
+    st.title("掲示板アプリ")
+
+    # 新規投稿の入力
+    
+    new_post_content = st.text_area("新規投稿", height=100)
+    new_post_title = st.text_input("ページ")
+    # 投稿ボタンが押された場合
+    if st.button("投稿する") and new_post_title and new_post_content:
+        new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
+        if "＠" in new_post_title or "＠" in new_post_content:
+            st.warning("禁止ワードが含まれています！")
+
+        save_post(new_post_title, new_post_content)
+        st.success("投稿が保存されました！")
+
+    # 保存された投稿の表示
+    posts = load_posts()
+    st.subheader("保存された投稿")
+
+    if not posts:
+        st.info("まだ投稿がありません。")
+
+if __name__ == "__main__":
+    main()
